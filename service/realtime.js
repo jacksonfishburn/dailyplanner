58ff5cc100688a9d3cab055182d2815ccde7a7dd
jacksonfishburn <jacksonfishburn@gmail.com>
2026-04-04 13:15:16 -0600

    starter helper functions

--- service/realtime.js
+++ service/realtime.js
@@ -5,3 +5,46 @@ const authCookieName = 'token';
 const userSockets = new Map();
 let socketServer;
 
+function parseCookies(cookieHeader) {
+	if (!cookieHeader) {
+		return {};
+	}
+
+	return cookieHeader
+		.split(';')
+		.map((pair) => pair.trim())
+		.filter(Boolean)
+		.reduce((acc, pair) => {
+			const [rawKey, ...rawValue] = pair.split('=');
+			if (!rawKey) {
+				return acc;
+			}
+
+			const key = decodeURIComponent(rawKey);
+			const value = decodeURIComponent(rawValue.join('='));
+			acc[key] = value;
+			return acc;
+		}, {});
+}
+
+function getTokenFromRequest(req) {
+	const cookies = parseCookies(req?.headers?.cookie);
+	return cookies[authCookieName];
+}
+
+function removeSocket(username, socket) {
+	if (!username || !socket) {
+		return;
+	}
+
+	const sockets = userSockets.get(username);
+	if (!sockets) {
+		return;
+	}
+
+	sockets.delete(socket);
+	if (sockets.size === 0) {
+		userSockets.delete(username);
+	}
+}
+
